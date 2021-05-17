402e7ac6c58db6bc7a2e5b35fcbd9caf48a4e2a8
mcxstillo <maia_castillo@dlsu.edu.ph>
2021-05-17 15:54:49 +0800

    fix for profile and recipebook

--- app/src/main/java/com/mobdeve/castillo/recipe_finder/RecipeBook.java
+++ app/src/main/java/com/mobdeve/castillo/recipe_finder/RecipeBook.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,13 +16,14 @@ import java.util.ArrayList;
 
 public class RecipeBook extends AppCompatActivity {
 
-
     //LIKED RECIPES/LANDING PAGE?
     //SEARCH PAGE
     private ArrayList<Recipe> recipes;
     private RecyclerView recipesRv;
     private ResultsAdapter adapter;
     private FloatingActionButton createBtn;
+    private Button profile;
+    private TextView notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +35,35 @@ public class RecipeBook extends AppCompatActivity {
         this.createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RecipeBook.this, CreateRecipe.class));
+                Intent create = new Intent(RecipeBook.this, CreateRecipe.class);
+                create.putExtra("TYPE", "CREATE");
+                startActivity(create);
             }
         });
 
-        LinearLayoutManager lm = new LinearLayoutManager(RecipeBook.this);
-        this.recipesRv.setLayoutManager(lm);
-        this.adapter = new ResultsAdapter(this.recipes);
-        this.recipesRv.setAdapter(this.adapter);
+        this.profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RecipeBook.this, profile.class));
+            }
+        });
+
+        if (recipes == null) {
+            recipesRv.setVisibility(View.GONE);
+            notice.setVisibility(View.VISIBLE);
+            profile.setVisibility(View.VISIBLE);
+        } else {
+            LinearLayoutManager lm = new LinearLayoutManager(RecipeBook.this);
+            this.recipesRv.setLayoutManager(lm);
+            this.adapter = new ResultsAdapter(this.recipes);
+            this.recipesRv.setAdapter(this.adapter);
+        }
     }
 
     private void init() {
         this.recipesRv = (RecyclerView) findViewById(R.id.recipesRv);
         this.createBtn = findViewById(R.id.createBtn);
+        this.notice = findViewById(R.id.noticeTv);
+        this.profile = findViewById(R.id.profileBtn);
     }
 }
